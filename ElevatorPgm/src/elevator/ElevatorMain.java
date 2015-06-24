a0272580606b8280930784f48d3fa18c94fd6bf9
omkarpg0931 <omkar0931@gmail.com>
2015-06-24 16:58:38 +0530

    version 2.1 redifened

--- ElevatorPgm/src/elevator/ElevatorMain.java
+++ ElevatorPgm/src/elevator/ElevatorMain.java
@@ -9,9 +9,9 @@ public class ElevatorMain {
 		Elevator ob=new Elevator();
 		Elevator obdup=new Elevator();
 		ob.openDoors();
-		System.out.print("\nPfloor=");
+		System.out.print("\nPresent floor=");
 		int a= 1;
-		System.out.print("\nDfloor=");
+		System.out.print("\nDefault floor=");
 		int b= 0;
 		ob.goToFloor(b);
 
