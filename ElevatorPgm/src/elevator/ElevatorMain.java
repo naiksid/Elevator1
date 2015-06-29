a275e51c3d81882d2db0a78c433305f392ba94df
omkarpg0931 <omkar0931@gmail.com>
2015-06-29 16:53:32 +0530

    Version 5

--- ElevatorPgm/src/elevator/ElevatorMain.java
+++ ElevatorPgm/src/elevator/ElevatorMain.java
@@ -1,7 +1,7 @@
 package elevator;
 
 import elevator.Elevator;
-
+import java.util.Scanner;
 public class ElevatorMain {
 
 	public static void main(String[] args) {
@@ -9,15 +9,30 @@ public class ElevatorMain {
 		Elevator ob=new Elevator();
 		Elevator obdup=new Elevator();
 		ob.openDoors();
-		//System.out.print("Present floor=");
-		int a= 1;
-		//System.out.print("Default floor=");
-		int b= 5;
-		ob.goToFloor(b);
-
-		obdup.goingUp();
-		obdup.goingDown();
-		ob.print(a);
-		obdup.closeDoors();
+		int current_floor=0;
+		while(current_floor>=0)
+		{
+			System.out.print("Present floor="+current_floor+"\n");
+			System.out.println("Which floor you want to go?\n");
+			Scanner obj = new Scanner(System.in);
+			int going_to= obj.nextInt();;
+			ob.goToFloor(going_to);
+			
+			if(current_floor<going_to){
+				obdup.goingDown();
+				ob.print(current_floor);
+			}
+			else if(current_floor>going_to){
+				obdup.goingUp();
+				ob.print(current_floor);
+			}
+			else if(current_floor==going_to)
+			{
+				ob.print(current_floor);
+			}
+			obdup.closeDoors();
+			current_floor=going_to;
+		}
 	}
+		
 }
